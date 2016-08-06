a6d866371fbd7bc5078f45131ec0d5182778c6f2
Xiaolei Li <lixiaoleice@gmail.com>
2016-08-06 22:08:42 +0800

    js

--- 易迅商城/scripts/base.js
+++ 易迅商城/scripts/base.js
@@ -1,3 +1,29 @@
 /**
  * Created by lixiaolei on 16/8/6.
  */
+function addLoadEvent(func) {
+    var oldonload = window.onload;
+    if (typeof window.onload != "function") {
+        window.onload = func();
+    }else {
+        window.onload = function () {
+            oldonload();
+            func();
+        }
+    }
+}
+function showListwrap() {
+    var list = document.getElementById("listwrap");
+    var itemswrap = document.getElementById("itemswrap");
+    var items = itemswrap.getElementsByTagName("dl");
+    for (var i=0; i<items.length; i++) {
+        items[i].onmouseover = function () {
+            list.setAttribute("class","");
+        }
+        items[i].onmouseout = function () {
+            list.setAttribute("class","hide");
+        }
+    }
+}
+
+addLoadEvent(showListwrap);
